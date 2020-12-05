723b63c8b40da19c71e45da3d511b3d2957db46f
Andrey <34249298+andreyc2018@users.noreply.github.com>
2020-12-05 18:21:46 -0500

    remove example code

--- lesson_2/home_work/countries/src/main/scala/CountriesApp.scala
+++ lesson_2/home_work/countries/src/main/scala/CountriesApp.scala
@@ -3,14 +3,7 @@ import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
 object CountriesApp extends App {
-//  case class Country(name: String, capital: String, area: Int, region: String)
-//  object Country {
-//    implicit val countryReads: Reads[Country] = {
-//      (JsPath \ "name")
-//    }
-//  }
-
-  //  def source = Source.fromURL(
+//  def source = Source.fromURL(
 //    "https://raw.githubusercontent.com/mledoze/countries/master/countries.json"
 //  )
 
@@ -38,42 +31,4 @@ object CountriesApp extends App {
   }
 
   println(countriesList.filter(_.region contains "Africa"))
-
-  def test_source = Source.fromFile(
-    "/home/andrey/Projects/learning/otus/otus_hadoop_spark/lesson_2/home_work/data/test.json"
-  )
-  case class FileName(size: String, datecreated: String, id: String, contenttype: String, filename: String)
-
-  implicit val test_reads: Reads[FileName] = (
-    (__ \ "size").read[String] and
-      (__ \ "date-created").read[String] and
-      (__ \ "id").read[String] and
-      (__ \ "content-type" \ 0).read[String] and
-      (__ \ "filename" \ "common").read[String]
-    )(FileName)
-
-  val test_json: JsValue = Json.parse(test_source.mkString(""))
-
-  val nameResult = test_json.validate[List[FileName]]
-//  println(nameResult)
-
-  val list = nameResult match {
-    case JsSuccess(list : List[FileName], _) => list
-    case e: JsError => {
-      println("Errors: " + JsError.toFlatForm(e).toString())
-      List()
-    }
-  }
-
-  println(list.filter(_.filename contains "morrow"))
-
-//  val json: JsValue = Json.parse(source.mkString(""))
-//  println(json.as[List])
-//  println(List(json).take(2))
-//  val json_a = Json.parse(source.mkString).as[JsArray]
-
-//  println(List(json_a))
-//  val name = (json \ "name")
-//  val name = (json \ "name" \ "common").get.toString()
-//  println(name)
 }
