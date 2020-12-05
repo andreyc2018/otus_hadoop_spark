46337c0cb2753d302459257d303698ff9a90e998
Andrey <34249298+andreyc2018@users.noreply.github.com>
2020-12-05 14:26:09 -0500

    add convinience scripts

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+function barf()
+{
+    local rc="$1"
+    shift
+    echo "$@"
+    exit "$rc"
+}
+
+HADOOP_SSH="hadoop"
+
+file="$1"
+
+[ -z "$file" ] && barf 1 "using: ${0##*/} <file>"
+
+echo "Sending "$file" to hadoop vm"
+scp "$file" hadoop:~/projects/
+
