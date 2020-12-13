f5120546690f085c301a3e9a4e1d9d0785d16ba6
Andrey <34249298+andreyc2018@users.noreply.github.com>
2020-12-12 23:20:22 -0500

    adding logging

--- lesson_3/solution/build.sbt
+++ lesson_3/solution/build.sbt
@@ -5,5 +5,6 @@ version := "0.1"
 scalaVersion := "2.13.4"
 
 libraryDependencies ++= Seq(
-  "org.apache.hadoop" % "hadoop-client" % "3.2.1"
+  "org.apache.hadoop" % "hadoop-client" % "3.2.1",
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
 )
--- lesson_3/solution/src/main/scala/CopyFiles.scala
+++ lesson_3/solution/src/main/scala/CopyFiles.scala
@@ -1,12 +1,16 @@
+import com.typesafe.scalalogging.Logger
 import org.apache.hadoop.conf._
 import org.apache.hadoop.fs._
+
 import java.net.URI
 
 object CopyFiles extends App {
+  val logger = Logger("name")
+  logger.info("Starting")
   val conf = new Configuration()
   val fileSystem = FileSystem.get(new URI("hdfs://localhost:9000"), conf)
 
-  val filename = "hello.csv"
+  val filename = "/stage/date=2020-12-03/part-0000.csv"
   val path = new Path(filename)
   val file: FSDataInputStream = fileSystem.open(path)
   println(file)
