a4ba7214f2cda7263dd0f12bad2e6440861616a2
Andrey <34249298+andreyc2018@users.noreply.github.com>
2021-01-03 23:00:16 -0500

    move converion to a function and use foreach

--- lesson_7/solution/kafka_writer/src/main/scala/edu/lesson_7/kafka/KafkaWriter.scala
+++ lesson_7/solution/kafka_writer/src/main/scala/edu/lesson_7/kafka/KafkaWriter.scala
@@ -1,32 +1,48 @@
 package edu.lesson_7.kafka
 
-import play.api.libs.json.{JsNumber, JsString}
+import play.api.libs.json.{JsNumber, JsString, JsValue, Json}
 
-class KafkaWriter(filename: String) {
-  printf("Will read CSV file \"%s\"\n", filename)
-  private val bufferedSource = io.Source.fromFile(filename)
+class KafkaWriter(file: String) {
+  printf("Will read CSV file \"%s\"\n", file)
+  private val filename: String = file
 
-  def write() {
-    var total = 0
-    var index = 0
-    for (line <- bufferedSource.getLines) {
-      if (index > 0) {
-        println(s"line: $line")
-        //    (?:,|\n|^)("(?:(?:"")*[^"]*)*"|[^",\n]*|(?:\n|$))
-        //    val result = line.split(",(?=([^\"]*\"[^\"]*\")*(?![^\"]*\"))")
-        //    result.foreach(f => println(f))
-        val Array(name, author, rating, reviews, price, year, genre) = line.split(",(?=([^\"]*\"[^\"]*\")*(?![^\"]*\"))").map(_.trim)
-        //      val book = Book(name, author, rating, reviews, price, year, genre)
-        //      println(s"array: $name $author $rating $reviews $price $year $genre")
-        //      println(book)
-        val m = Map("name" -> JsString(name), "author" -> JsString(author),
-          "rating" -> JsNumber(rating.toDouble), "reviews" -> JsNumber(reviews.toInt),
-          "price" -> JsNumber(price.toDouble), "year" -> JsNumber(year.toDouble), "genre" -> JsString(genre))
-        println(s"m = $m")
-      }
-      index += 1
-      total += 1
-    }
-    println(s"total = $total")
+  def lineToMap(line: String): Map[String, JsValue with Serializable] = {
+    val Array(name, author, rating, reviews, price, year, genre) = line.split(",(?=([^\"]*\"[^\"]*\")*(?![^\"]*\"))").map(_.trim)
+    Map("name" -> JsString(name), "author" -> JsString(author),
+      "rating" -> JsNumber(rating.toDouble), "reviews" -> JsNumber(reviews.toInt),
+      "price" -> JsNumber(price.toDouble), "year" -> JsNumber(year.toDouble), "genre" -> JsString(genre))
+  }
+
+  def write(): Unit = {
+    val bufferedSource = io.Source.fromFile(filename)
+//    var total = 0
+//    var index = 0
+    bufferedSource.getLines().drop(1).foreach(line => {
+      val m = lineToMap(line)
+      println(s"m = $m")
+      val dataToWrite: JsValue = Json.toJson(m)
+      println(s"json: $dataToWrite")
+    })
+
+//    for (line <- bufferedSource.getLines) {
+//      if (index > 0) {
+//        println(s"line: $line")
+//        //    (?:,|\n|^)("(?:(?:"")*[^"]*)*"|[^",\n]*|(?:\n|$))
+//        //    val result = line.split(",(?=([^\"]*\"[^\"]*\")*(?![^\"]*\"))")
+//        //    result.foreach(f => println(f))
+//        val Array(name, author, rating, reviews, price, year, genre) = line.split(",(?=([^\"]*\"[^\"]*\")*(?![^\"]*\"))").map(_.trim)
+//        //      val book = Book(name, author, rating, reviews, price, year, genre)
+//        //      println(s"array: $name $author $rating $reviews $price $year $genre")
+//        //      println(book)
+//        val m = Map("name" -> JsString(name), "author" -> JsString(author),
+//          "rating" -> JsNumber(rating.toDouble), "reviews" -> JsNumber(reviews.toInt),
+//          "price" -> JsNumber(price.toDouble), "year" -> JsNumber(year.toDouble), "genre" -> JsString(genre))
+//        println(s"m = $m")
+//      }
+//      index += 1
+//      total += 1
+//    }
+//    println(s"total = $total")
+    bufferedSource.close()
   }
 }
