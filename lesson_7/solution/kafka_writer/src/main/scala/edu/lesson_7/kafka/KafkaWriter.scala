792f28feb0ef8fb57619ecbb35f5eef445da4da4
Andrey <34249298+andreyc2018@users.noreply.github.com>
2021-01-03 23:01:13 -0500

    delete unused code

--- lesson_7/solution/kafka_writer/src/main/scala/edu/lesson_7/kafka/KafkaWriter.scala
+++ lesson_7/solution/kafka_writer/src/main/scala/edu/lesson_7/kafka/KafkaWriter.scala
@@ -15,8 +15,6 @@ class KafkaWriter(file: String) {
 
   def write(): Unit = {
     val bufferedSource = io.Source.fromFile(filename)
-//    var total = 0
-//    var index = 0
     bufferedSource.getLines().drop(1).foreach(line => {
       val m = lineToMap(line)
       println(s"m = $m")
@@ -24,25 +22,6 @@ class KafkaWriter(file: String) {
       println(s"json: $dataToWrite")
     })
 
-//    for (line <- bufferedSource.getLines) {
-//      if (index > 0) {
-//        println(s"line: $line")
-//        //    (?:,|\n|^)("(?:(?:"")*[^"]*)*"|[^",\n]*|(?:\n|$))
-//        //    val result = line.split(",(?=([^\"]*\"[^\"]*\")*(?![^\"]*\"))")
-//        //    result.foreach(f => println(f))
-//        val Array(name, author, rating, reviews, price, year, genre) = line.split(",(?=([^\"]*\"[^\"]*\")*(?![^\"]*\"))").map(_.trim)
-//        //      val book = Book(name, author, rating, reviews, price, year, genre)
-//        //      println(s"array: $name $author $rating $reviews $price $year $genre")
-//        //      println(book)
-//        val m = Map("name" -> JsString(name), "author" -> JsString(author),
-//          "rating" -> JsNumber(rating.toDouble), "reviews" -> JsNumber(reviews.toInt),
-//          "price" -> JsNumber(price.toDouble), "year" -> JsNumber(year.toDouble), "genre" -> JsString(genre))
-//        println(s"m = $m")
-//      }
-//      index += 1
-//      total += 1
-//    }
-//    println(s"total = $total")
     bufferedSource.close()
   }
 }
