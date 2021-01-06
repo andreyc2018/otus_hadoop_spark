f161e3ca9a0095b2c687f7c57605efc2631f9458
Andrey <34249298+andreyc2018@users.noreply.github.com>
2021-01-05 21:38:55 -0500

    Remove comments

--- lesson_7/solution/kafka_writer/src/test/scala/edu/lesson_7/kafka/KafkaWriterTest.scala
+++ lesson_7/solution/kafka_writer/src/test/scala/edu/lesson_7/kafka/KafkaWriterTest.scala
@@ -18,27 +18,20 @@ class KafkaWriterTest extends AnyFunSuite with BeforeAndAfterEach {
         Map("price" -> 12, "year" -> 2019, "rating" -> 4.8, "reviews" -> 7665)
       )
     )
-//      "{ \"name\": \"\", \"author\": \"\", \"rating\": 0, \"reviews\": 0, \"price\": 0, \"year\": 1234, \"genre\": \"\" }",
-//      "{ \"name\": \"\", \"author\": \"\", \"rating\": 0, \"reviews\": 0, \"price\": 0, \"year\": 1234, \"genre\": \"\" }")
+
     val bufferedSource = io.Source.fromString(data)
     val writer: KafkaWriter = new KafkaWriter("inputFile")
-
-    println(expectedObjects)
 
     var idx = 0
     for (line <- bufferedSource.getLines().drop(1)) {
       val dataToWrite = writer.lineToJson(line)
-      println(s"json: $dataToWrite")
       val expected = expectedObjects(idx)
-      println(expected(0))
       for ((k,v) <- expected(0)) {
         val a = (dataToWrite \ k).as[String]
-        println(s"k = $k, v = $v, a = $a")
         assert(v == a)
       }
       for ((k,v) <- expected(1)) {
         val a = (dataToWrite \ k).as[Double]
-        println(s"k = $k, v = $v, a = $a")
         assert(v == a)
       }
       idx += 1
