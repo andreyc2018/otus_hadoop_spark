7b5cd11a4514c9f2e03df8654b6cbc6ca10ed668
Andrey <34249298+andreyc2018@users.noreply.github.com>
2021-01-18 14:04:13 -0500

    write time and counter as a key

--- lesson_7/solution/kafka_writer/src/main/scala/edu/lesson_7/kafka/Writer.scala
+++ lesson_7/solution/kafka_writer/src/main/scala/edu/lesson_7/kafka/Writer.scala
@@ -4,7 +4,9 @@ import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.apache.kafka.common.serialization.{LongSerializer, StringSerializer}
 import play.api.libs.json.{JsNumber, JsString, JsValue, Json}
 
+import java.time.Instant
 import java.util.Properties
+import scala.util.Random
 
 object Writer {
 
@@ -18,15 +20,22 @@ object Writer {
   def fromFile(filename: String, topic: String): Unit = {
     val props = new Properties()
     props.put("bootstrap.servers", "localhost:29092")
+//    props.put("transactional.id", "books")
     val producer = new KafkaProducer(props, new LongSerializer, new StringSerializer)
-
+//    producer.initTransactions()
+//    producer.beginTransaction()
+    var counter = 1
     val bufferedSource = io.Source.fromFile(filename)
     bufferedSource.getLines().drop(1).foreach(line => {
       val dataToWrite = lineToJson(line)
       val data = Json.stringify(dataToWrite)
-      val key = data.hashCode()
+      val key = Instant.now.toEpochMilli + counter
       producer.send(new ProducerRecord(topic, key, data))
+      println(s"sending key = ${key}")
+      counter += 1
     })
+
+//    producer.commitTransaction()
 
     producer.close()
     bufferedSource.close()
