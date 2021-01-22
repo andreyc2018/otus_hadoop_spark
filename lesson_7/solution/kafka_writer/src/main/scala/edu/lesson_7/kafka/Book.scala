5ddf27b9441445baa06c3c4fd21138ec5f830d23
Andrey <34249298+andreyc2018@users.noreply.github.com>
2021-01-22 00:15:21 -0500

    Use bookRecord instead of Book

--- lesson_7/solution/kafka_writer/src/main/scala/edu/lesson_7/kafka/Book.scala
+++
@@ -1,4 +0,0 @@
-package edu.lesson_7.kafka
-
-case class Book(name: String, author: String, userRating: String, review: String,
-                price: String, year: String, genre: String)
---
+++ lesson_7/solution/kafka_writer/src/main/scala/edu/lesson_7/kafka/BookRecord.scala
@@ -0,0 +1,3 @@
+package edu.lesson_7.kafka
+
+case class BookRecord(offset: Long, data: String)
