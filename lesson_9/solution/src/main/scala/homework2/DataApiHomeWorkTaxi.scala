b34a809364ba2098841faae25eb25c7f01e16caa
Andrey <34249298+andreyc2018@users.noreply.github.com>
2021-01-29 14:21:09 -0500

    delete submodules, start lesson #9

---
+++ lesson_9/solution/src/main/scala/homework2/DataApiHomeWorkTaxi.scala
@@ -0,0 +1,33 @@
+package homework2
+
+import org.apache.spark.sql.SparkSession
+
+object DataApiHomeWorkTaxi extends App {
+
+  val spark = SparkSession.builder()
+    .appName("Joins")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  val driver = "org.postgresql.Driver"
+  val url = "jdbc:postgresql://localhost:5432/otus"
+  val user = "docker"
+  val password = "docker"
+
+
+  val taxiFactsDF = spark.read.load("src/main/resources/data/yellow_taxi_jan_25_2018")
+  taxiFactsDF.printSchema()
+  taxiFactsDF.show(10)
+  println(taxiFactsDF.count())
+
+
+  /**
+   * Задание написать код, который будет делать следующее:
+   *
+   * 1. DataFrame: Какие районы самые популярные для заказов? Результат в Parquet.
+   * 2. RDD: В какое время происходит больше всего вызовов? Результат в txt файл c пробелами.
+   * 3. DataSet: Как происходит распределение заказов? Результат записать в базу данных Postgres.
+   */
+
+}
+
