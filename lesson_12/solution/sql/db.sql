d1c8c5b0952c9311839b21b1e093abe8daa3f82f
Andrey <34249298+andreyc2018@users.noreply.github.com>
2021-02-21 20:08:14 -0500

    prepare project for the tests

--- lesson_12/solution/sql/db.sql
+++ lesson_12/solution/sql/db.sql
@@ -1,14 +1,3 @@
 
 DROP DATABASE IF EXISTS otus;
 CREATE DATABASE otus;
-
-\connect otus
-
-CREATE TABLE trip_stats (
-    trips       INT,
-    avg_dist    REAL,
-    max_dist    REAL,
-    min_dist    REAL,
-    stddev_dist REAL
-);
-
