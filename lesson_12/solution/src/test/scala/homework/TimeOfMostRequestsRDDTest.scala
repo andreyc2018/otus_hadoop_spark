07581625f39d44696ea537aba34debd43f2a7cd1
Andrey <34249298+andreyc2018@users.noreply.github.com>
2021-03-05 13:48:31 -0500

    add before and after methods

--- lesson_12/solution/src/test/scala/homework/TimeOfMostRequestsRDDTest.scala
+++ lesson_12/solution/src/test/scala/homework/TimeOfMostRequestsRDDTest.scala
@@ -2,16 +2,29 @@ package homework
 
 import homework.DataApiHomeWorkTaxi.readStats
 import org.apache.spark.sql.SparkSession
-
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.AnyFlatSpec
 
-class TimeOfMostRequestsRDDTest extends AnyFlatSpec {
+class TimeOfMostRequestsRDDTest extends AnyFlatSpec with BeforeAndAfterAll {
 
-  implicit val spark = SparkSession
-    .builder()
-    .config("spark.master", "local")
-    .appName("Test №1: TimeOfMostRequests")
-    .getOrCreate()
+  protected var spark: SparkSession = _
+
+  override def beforeAll(): Unit = {
+    spark = SparkSession
+      .builder()
+      .config("spark.master", "local")
+      .appName("Test №1: TimeOfMostRequests")
+      .getOrCreate()
+
+    println(s"Created spark context $spark")
+
+    super.beforeAll()
+  }
+  override def afterAll(): Unit = {
+    println(s"Closing spark context $spark")
+    spark.close()
+    super.afterAll()
+  }
 
   it should "upload and find time of most requests" in {
     val taxiFactsFile = "src/main/resources/data/yellow_taxi_jan_25_2018"
