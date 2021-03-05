68e36cf4f09c3b6921bea26269fe31ad990559cd
Andrey <34249298+andreyc2018@users.noreply.github.com>
2021-03-05 18:00:25 -0500

    test with SharedSparkSession

---
+++ lesson_12/solutions/task_3/src/main/scala/homework/MostPopularDF.scala
@@ -0,0 +1,44 @@
+package homework
+
+import homework.DataApiHomeWorkTaxi.{init, readInfo, readStats, taxiFactsFile, taxiInfoFile}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.col
+
+object MostPopularDF {
+
+  def run(): Unit = {
+
+    println("DataFrame: Какие районы самые популярные для заказов?")
+
+    implicit val spark = init()
+
+    val taxiFactsDF = readStats(taxiFactsFile)
+    val taxiInfoDF  = readInfo(taxiInfoFile)
+
+    val mostPopularDF = process(taxiFactsDF, taxiInfoDF)
+
+    val destDir = "src/main/resources/data/most_popular_pu_location"
+    save(destDir, mostPopularDF)
+
+    val readBackDF = readStats(destDir)
+    readBackDF.show()
+
+    spark.close()
+  }
+
+  def process(facts: DataFrame, info: DataFrame): DataFrame = {
+    facts
+      .join(info, col("PULocationID") <=> col("LocationID"))
+      .groupBy(col("Borough"))
+      .count
+      .orderBy(col("count").desc)
+  }
+
+  def save(dest: String, data: DataFrame): Unit = {
+    data
+      .repartition(1)
+      .write
+      .mode("overwrite")
+      .parquet(dest)
+  }
+}
